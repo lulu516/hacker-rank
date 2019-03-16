f1ff03352b58fb1b2d6277bad16e02b336d136e8
lulu516 <lulu19910516@gmail.com>
2019-03-16 13:22:20 +0100

    a very big sum

---
+++ src/AVeryBigSum.kt
@@ -0,0 +1,21 @@
+/***
+ * https://www.hackerrank.com/challenges/a-very-big-sum/problem
+ */
+import java.util.*
+
+// Complete the aVeryBigSum function below.
+fun aVeryBigSum(ar: Array<Long>): Long {
+    return ar.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
